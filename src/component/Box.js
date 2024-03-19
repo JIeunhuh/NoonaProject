24af144319d2ff327ac96604a7e5e74018895542
Jieun Huh <entks301@naver.com>
2024-03-19 22:43:28 +0900

    alt 추가

--- src/component/Box.js
+++ src/component/Box.js
@@ -4,7 +4,7 @@ export const Box = ({title,item}) => {
   return (
       <div className='box'>
         <div style={{textAlign : 'center'}}>{title}</div>
-        <img className="pic" src={item && item.img}/>
+        <img className="pic" src={item && item.img} alt='가위바위보'/>
         <div style={{fontWeight : 'normal', fontSize:'45px'}}>You WIN !</div>
       </div>
   )
