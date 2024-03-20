4e4332d8381496a6fbcec7752e323c623628cf1f
Jieun Huh <entks301@naver.com>
2024-03-21 00:09:08 +0900

    === instead of ==

--- src/component/Box.js
+++ src/component/Box.js
@@ -2,7 +2,7 @@ import React from 'react'
 export const Box = ({ title, item, res }) => {
   console.log(item)
   return (
-    <div className='box' style={res == "WIN!" ? { borderColor: 'green' } : res == "Lose.." ? { borderColor: 'red' } : { borderColor: 'pink' }}>
+    <div className='box' style={res === "WIN!" ? { borderColor: 'green' } : res === "Lose.." ? { borderColor: 'red' } : { borderColor: 'pink' }}>
       <div style={{ textAlign: 'center' }}>{title}</div>
       {item === null? (<i class="nes-octocat animate"></i>) : (<img className="pic" src={item.img} alt='가위바위보' />)}
       <div style={{ fontWeight: 'normal', fontSize: '45px' }}>{item === null ? "Let's Play!" : res}</div>
