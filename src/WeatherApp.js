59709a6428a3078396c97aa9b739b10b925423a9
Jieun Huh <entks301@naver.com>
2024-03-25 01:37:13 +0900

    현재 위치 클릭이벤트 추가 로딩스피너 추가

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -2,8 +2,15 @@ import React, { useEffect, useState } from 'react'
 import './WeatherApp.css'
 import { WeatherBox } from './component/WeatherBox'
 import { WeatherBtn } from './component/WeatherBtn'
+import { ClipLoader } from 'react-spinners'
 export const WeatherApp = () => {
+    // weather data
     const [weather, setWeather] = useState(null)
+    // change city
+    const [city, setCity] = useState('');
+    // loading spinner
+    const [loading, setLoading] = useState(false)
+    const mykey = '9a275cedf6cd3d4919d4407baab53244'
     // get current location info
     const getCurLocation = () => {
         //  현재 위치정보 가져오기
@@ -13,27 +20,55 @@ export const WeatherApp = () => {
         navigator.geolocation.getCurrentPosition((position) => {
             let lat = position.coords.latitude
             let long = position.coords.longitude
-            const mykey = '9a275cedf6cd3d4919d4407baab53244'
             let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${mykey}&units=metric`
             fetch(url)
                 .then((res) => res.json())
                 .then((data) => setWeather(data))
                 .catch((err) => console.log(err))
+            setLoading(false)
         });
 
     }
-
+    // 버튼 클릭해서 원하는 도시의 날씨 가져오기 
+    const getSelCity = () => {
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${mykey}&units=metric`
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => setWeather(data))
+            .catch((err) => console.log(err))
+        setLoading(false)
+    }
+    const cities = ['Current Location', 'Busan', 'Tokyo', 'New york', 'Shanghai']
     useEffect(() => {
-        getCurLocation()
+        if (city === "" || city === 'Current Location') {
+            setLoading(true)
+            getCurLocation()
+        }
+        else {
+            setLoading(true)
+            getSelCity()
+        }
+    }, [city]);
 
-    }, []);
     return (
         <>
-            <div style={{fontSize : '40px', color:'purple'}}>오늘의 날씨</div>
-            <div className='container'>
-                <WeatherBox weather={weather}/>
-                <WeatherBtn />
-            </div>
+
+            <div style={{ fontSize: '40px', color: 'purple' }}>오늘의 날씨</div>
+            {loading ?
+                (<div className='container'>
+                    <ClipLoader
+                        color={'rgb(220, 127, 204)'}
+                        loading={loading}
+                        size={150}
+                        aria-label="Loading Spinner"
+                        data-testid="loader"
+                    />
+                </div>) :
+                (<div className='container'>
+                    <WeatherBox weather={weather} />
+                    <WeatherBtn cities={cities} setCity={setCity} />
+                </div>)}
+
         </>
     )
 }
--- src/component/WeatherBtn.js
+++ src/component/WeatherBtn.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
 
-export const WeatherBtn = () => {
+export const WeatherBtn = ({cities,setCity}) => {
     
+    const clickCity=(city)=>{
+      setCity(city)
+    }
   return (
-    <div style={{margin : '3%'}}>
-        <Button variant='success'>Current Location</Button>
-        <Button variant='success'>New York</Button>
-        <Button variant='success'>LA</Button>
-        <Button variant='success'>Shanhai</Button>
+    <div style={{margin : '1.5%', width : '800px', display:'flex', justifyContent:'center'}}>
+        {cities.map((item,idx)=>
+          <Button variant='success' key={idx} onClick={()=>clickCity(item)}>{item}</Button>
+        )}
     </div>
   )
 }
