e4502f6ef858cbe6c0ba32708e2939fe6162ef54
Jieun Huh <entks301@naver.com>
2024-03-25 23:20:42 +0900

    버튼 클릭하면 색 변경되도록 수정

--- src/component/WeatherBtn.js
+++ src/component/WeatherBtn.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import { useState } from 'react';
 import { Button } from 'react-bootstrap'
 
 export const WeatherBtn = ({cities,setCity}) => {
+    const [btnColor, setBtnColor] = useState(null);
     
-    const clickCity=(city)=>{
+    const clickCity=(city,idx)=>{
+      setBtnColor(idx)
       setCity(city)
     }
   return (
     <div style={{margin : '1.5%', width : '800px', display:'flex', justifyContent:'center'}}>
         {cities.map((item,idx)=>
-          <Button variant='success' key={idx} onClick={()=>clickCity(item)}>{item}</Button>
+          <Button style={{backgroundColor:btnColor===idx ? 'rgb(62, 86, 59)':'green'}} className='btn' variant='success' key={idx} onClick={()=>clickCity(item,idx)}>{item}</Button>
         )}
     </div>
   )
