45bc951ad30c13ad85d650b5452c50a9507af5ff
Jieun Huh <entks301@naver.com>
2024-03-29 00:35:52 +0900

    서버 연동 됨

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -4,8 +4,8 @@ import { ProductCard } from '../component/ProductCard'
 export const ProductAll = () => {
     const [items, setItems] = useState([])
     const getAllProducts=()=>{
-        let searchQurey = query.get("q") || "";
-        let url = `http://my-json-server.typicode.com/JIeunhuh/NoonaProject/products?q=${searchQurey}`
+        // let searchQurey = query.get("q") || "";
+        let url = `http://my-json-server.typicode.com/JIeunhuh/NoonaProject/products`
         fetch(url)
             .then((res)=> res.json())
             .then((data)=>setItems(data))
