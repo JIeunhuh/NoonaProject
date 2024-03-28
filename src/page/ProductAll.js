3e6a202c0eb7e1959a21f16d5bdc6cc64a66b7ee
Jieun Huh <entks301@naver.com>
2024-03-29 00:13:51 +0900

    url 분기점 제거

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -4,7 +4,7 @@ import { ProductCard } from '../component/ProductCard'
 export const ProductAll = () => {
     const [items, setItems] = useState([])
     const getAllProducts=()=>{
-        let url = 'http://my-json-server.typicode.com/JIeunhuh/NoonaProject/tree/shoppingmall/products'
+        let url = 'http://my-json-server.typicode.com/JIeunhuh/NoonaProject/products'
         fetch(url)
             .then((res)=> res.json())
             .then((data)=>setItems(data))
