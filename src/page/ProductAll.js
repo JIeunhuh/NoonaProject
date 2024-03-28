6684b4fd4b79cedcdfc3807f4def91b95eea7694
Jieun Huh <entks301@naver.com>
2024-03-29 01:25:50 +0900

    async await

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -3,11 +3,11 @@ import { ProductCard } from '../component/ProductCard'
 
 export const ProductAll = () => {
     const [items, setItems] = useState([])
-    const getAllProducts=()=>{
+    const getAllProducts=async ()=>{
         // const [query, setQuery] = useSearchParams();
         // let searchQurey = query.get("q") || "";
         let url = `http://my-json-server.typicode.com/JIeunhuh/NoonaProject/products`
-        fetch(url)
+        await fetch(url)
             .then((res)=> res.json())
             .then((data)=>setItems(data))
             .catch((err)=>console.log(err))
