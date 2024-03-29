6341e75f18c239b09b345ba333f549adb3ffe58c
Jieun heo <112235808+JIeunhuh@users.noreply.github.com>
2024-03-30 00:37:41 +0900

    Update ProductAll.js

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -5,16 +5,14 @@ import { useSearchParams } from 'react-router-dom'
 export const ProductAll = () => {
     const [items, setItems] = useState([])
     const [query] = useSearchParams()
-    const getAllProducts = async () => {
+
+    useEffect(() => {
         let searchQurey = query.get("q") || ''
         let url = `https://my-json-server.typicode.com/JIeunhuh/NoonaProject/products?q=${searchQurey}`
         await fetch(url)
             .then((res) => res.json())
             .then((data) => setItems(data))
             .catch((err) => console.log(err))
-    }
-    useEffect(() => {
-        getAllProducts()
     }, [query])
     return (
         <div>
